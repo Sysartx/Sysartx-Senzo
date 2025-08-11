ad2764c7f1d63f8dbd18e7cbf8b1e27a2418982e
ShariqShahood <shariqshahood43@gmail.com>
2025-08-11 09:31:53 -0700

    .next remove

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2DInmFmQLK5Kna4qwoipqO5wjXtav2rQYIcWjl2hAdA=\"\n}"
--- frontend/.next/static/chunks/src_app_(admin)_documentation_page_tsx_83340e0c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_399e8e6a._.js"
-],
-    source: "dynamic"
-});
