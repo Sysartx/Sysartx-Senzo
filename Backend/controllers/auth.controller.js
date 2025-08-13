574a20a973f24feeccd06f20d1c4fe00c4af16b4
Soha Raees <souharaees@gmail.com>
2025-08-13 22:57:10 +0500

    Implement user authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,14 +1,101 @@
-export const signIn = async (req, res) => {
-  try {
-  } catch (error) {}
-};
+import { db } from "../lib/db.js";
+import { users } from "../lib/schema.js";
+import { eq } from "drizzle-orm";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
 
 export const signOut = async (req, res) => {
-  try {
-  } catch (error) {}
+   try {
+    res.status(200).json({
+      success: true,
+      message: "User signed out",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
+
+
 
 export const signUp = async (req, res) => {
   try {
-  } catch (error) {}
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (existingUser.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await db.insert(users).values({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    console.error("Sign-up error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    const userRecord = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
+
+    if (userRecord.length === 0) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const user = userRecord[0];
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+
+    const token =  generateToken(user.id, user.role);
+
+   
+    return res.status(200).json({
+      message: "logged in successfully",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        image: user.image,
+        emailVerified: user.emailVerified,
+      }
+    });
+
+  } catch (error) {
+    console.error("Sign-in error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
